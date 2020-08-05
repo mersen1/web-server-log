e65eedcb8986867db3f2c371580f54cb5c22dc68
Sergey Kucherov <sergey.kucherov94@gmail.com>
2020-08-05 19:54:40 +0300

    add implementation of UniqPresenter

--- lib/web_server_log/presenters/line_entities/uniq_presenter.rb
+++ lib/web_server_log/presenters/line_entities/uniq_presenter.rb
@@ -4,6 +4,9 @@ module WebServerLog
   module Presenters
     module LineEntities
       class UniqPresenter < Base
+        def execute
+          "#{line_entity.path} #{line_entity.visits} unique views"
+        end
       end
     end
   end
