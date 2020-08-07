7a8c453c1f6bbe11d6a7a423c6a380494a9ed364
Sergey Kucherov <sergey.kucherov94@gmail.com>
2020-08-07 15:10:03 +0300

    change #add_if_not_exists of line_repository_spec.rb for the new implementation

--- spec/web_server_log/repositories/line_repository_spec.rb
+++ spec/web_server_log/repositories/line_repository_spec.rb
@@ -5,8 +5,9 @@ describe WebServerLog::Repositories::LineRepository do
 
   describe '#add_if_not_exists' do
     let(:product_path) { '/index' }
+    let(:product_ip) { '184.123.665.067' }
 
-    subject { repository.add_if_not_exists(product_path) }
+    subject { repository.add_if_not_exists(product_path, product_ip) }
 
     context 'when such product_path does not exist' do
       it { expect { subject }.to change(repository.products, :count).by(1) }
