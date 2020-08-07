a5f1e9690fad1e81e8839d3d439062e08cd2c8db
Sergey Kucherov <sergey.kucherov94@gmail.com>
2020-08-07 14:46:32 +0300

    add attribute ip to line_entity_spec.rb

--- spec/web_server_log/entities/line_entity_spec.rb
+++ spec/web_server_log/entities/line_entity_spec.rb
@@ -2,11 +2,12 @@
 
 describe WebServerLog::Entities::LineEntity do
   describe '.new' do
+    let(:ip) { '184.123.665.067' }
     let(:path) { '/index' }
 
-    subject { described_class.new(path) }
+    subject { described_class.new(path, ip) }
 
-    it { is_expected.to have_attributes(path: path, visits: 0) }
+    it { is_expected.to have_attributes(path: path, ip: ip, visits: 0) }
 
     context 'when path is invalid' do
       # TODO
